ab7d945b223fb90bfab411854966a5c7ed08bc7e
Dipendra6377 <91822875+Dipendra6377@users.noreply.github.com>
2022-12-21 15:49:01 +0530

    Create check_if_array_is_sorted.java

---
+++ 22 December 2022/Question 1 to Question 10/check_if_array_is_sorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    boolean arraySortedOrNot(int[] arr, int n) {
+        // code here
+        for(int i=0;i<n-1;i++){
+            if(arr[i]<=arr[i+1]){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
